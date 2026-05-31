fd134f1be1d53e867f589ed821be1f09cc0daffa
eraera06 <eracaushi0@gmail.com>
2026-05-31 18:30:01 +0200

    Create paypal.php

---
+++ src/config/paypal.php
@@ -0,0 +1,5 @@
+<?php
+define("PAYPAL_CLIENT_ID", "VENDOS_CLIENT_ID_KETU");
+define("PAYPAL_CLIENT_SECRET", "VENDOS_CLIENT_SECRET_KETU");
+define("PAYPAL_BASE_URL", "https://api-m.sandbox.paypal.com");
+?>
