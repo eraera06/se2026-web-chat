2f49d4102dab393158e31bdf266c378cc7c60459
eraera06 <eracaushi0@gmail.com>
2026-05-31 18:29:36 +0200

    Update db.php

--- src/config/db.php
+++ src/config/db.php
@@ -1,1 +1,20 @@
+<?php
+$host = "127.0.0.1";
+$user = "root";
+$pass = "";
+$dbname = "webchat";
+$port = 3307;
 
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
+
+if ($conn->connect_error) {
+    header("Content-Type: application/json");
+    echo json_encode([
+        "status" => "error",
+        "message" => "Nuk u lidh me databazen: " . $conn->connect_error
+    ]);
+    exit;
+}
+
+$conn->set_charset("utf8mb4");
+?>
